d34b2cfc25a0db511e2adb28ceafc5d403970832
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-15 13:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ 
+const student = {
+  name: "John",
+};
+
+ function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+ console.log(getKeys(student)); // Output: ["name"]
+
+ window.student = student;
+window.getKeys = getKeys;
