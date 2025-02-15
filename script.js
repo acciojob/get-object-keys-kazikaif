2e8e2acf0810d5939278d77b96570a19f3ef909f
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-15 13:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Create student object
-const student = {
-  name: "John",
-  age: 30,
-  city: "New York"
+// Extend Object prototype with getKeys
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
 };
 
-// Standalone function to get keys
-function getKeys(obj) {
-  return Object.keys(obj);
-}
+const student = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
 
 // Test output
-console.log(getKeys(student)); // Output: ["name", "age", "city"]
+console.log(student.getKeys()); // ["name", "age", "city"]
 
 // Do not change the code below
 window.student = student;
-window.getKeys = getKeys;
